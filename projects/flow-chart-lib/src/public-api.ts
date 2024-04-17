6dd9bbc93fe6fb1e15eeba75af0c2efe2f0adc5b
Dinesh-kumr <dineshkmrc24@gmail.com>
2024-04-17 16:53:59 +0530

    update version

--- projects/flow-chart-lib/src/public-api.ts
+++ projects/flow-chart-lib/src/public-api.ts
@@ -1,6 +1,6 @@
 /*
  * Public API Surface of flow-chart-lib
  */
-export * from './lib/flow-chart-lib.service';
-export * from './lib/flow-chart-lib.component';
+// export * from './lib/flow-chart-lib.service';
+// export * from './lib/flow-chart-lib.component';
 export * from './lib/flow-chart/flow-chart.component';
